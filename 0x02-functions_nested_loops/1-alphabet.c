3cf55cba58919ae7240a3b4b434ac1d968253b5b
EBEN1990 <acquahkofiebenezer@gmail.com>
2023-02-21 06:45:58 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alpha in lowercase
+ * Retuen: void
+ */
+void print_alphabet(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+
+	_putchar('\n');
+}
