462c1f6932549e960b1039cbe1ea5cc0118e6983
EBEN1990 <acquahkofiebenezer@gmail.com>
2023-02-22 03:22:17 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,17 +1,22 @@
-#include <stdio.h>
+#include <stdio>
+
 /**
-  * main - computes and prints the sum of all the multiples of
-  * 3 or 5 below 1024.
-  * Return: Nothing.
-  */
+ * main - prints the sum of all multiples of 3 or 5 up to 1024
+ * Return: always (Success)
+ */
 
 int main(void)
 {
-        int i, res;
+	int i, z = 0;
 
-        for (i = 0; i < 1024; i++)
-                if (i % 3 == 0 || i % 5 == 0)
-                        res += i;
-        printf("%d\n", res);
-        return (0);
+	while (i < 1024)
+	{
+	if ((i % 3 == 0) || (i % 5 == 0))
+	{
+	z += i;
+	}
+	i++;
+	}
+	printf("%d\n", z);
+	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -14,5 +14,6 @@ void print_to_98(int n);
 int _putchar(char c);
 void print_alphabet(void);
 int add(int, int);
+void print_times_table(int n);
 
 #endif
