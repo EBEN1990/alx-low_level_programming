3e277d62be9c0dda3c4d5ec20f47467f480fb74a
EBEN1990 <acquahkofiebenezer@gmail.com>
2023-02-27 12:49:23 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,51 +2,23 @@
 #include <stdlib.h>
 #include <time.h>
 
-/**
- * main - Generates random valid password for the
- * program 101-crackme
- * Return: Always 0
- */
-int main(void)
-{
-	char password[84];
-	int index = 0, sum = 0, diff_half1, diff_half2;
+#define PASSWORD_LENGTH 10
 
-	srand(time(0));
-
-	while (sum < 27772)
-	{
-		password[index] = 33 + rand() % 94;
-		sum += password[index++];
-	}
-	password[index] = '\0';
-
-	if (sum != 2772)
-	{
-		diff_half1 = (sum - 2772) / 2;
-		diff_half2 = (sum - 2772) / 2;
-
-		if ((sum - 2772) % 2 != 0)
-		diff_half1++;
-
-		for (index = 0; password[index]; index++)
-		{
-			if (password[index] >= (33 + diff_half1))
-			{
-				password[index] -= diff_half1;
-				break;
-			}
-		}
-
-		for (index = 0; password[index]; index++)
-		{
-			if (password[index] >= (33 + diff_half2))
-			{
-				password[index] -= diff_half2;
-				break;
-			}
-		}
-	}
-	printf("%s", password);
-	return (0);
+int main(void) {
+    char password[PASSWORD_LENGTH + 1];
+    int i;
+    
+    // Seed the random number generator with the current time
+    srand(time(NULL));
+    
+    // Generate a random password
+    for (i = 0; i < PASSWORD_LENGTH; i++) {
+        password[i] = rand() % 94 + 33;
+    }
+    password[PASSWORD_LENGTH] = '\0';
+    
+    // Print the password
+    printf("%s\n", password);
+    
+    return 0;
 }
